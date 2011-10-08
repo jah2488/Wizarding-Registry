99c14b9c763625cf004c99c1a35b83810ded8e33
Justin Herrick <Justin@justinherrick.com>
2011-10-08 00:02:52 -0400

    changes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111003000553) do
+ActiveRecord::Schema.define(:version => 20111005230213) do
 
   create_table "users", :force => true do |t|
     t.datetime "created_at"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(:version => 20111003000553) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "real_name"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
