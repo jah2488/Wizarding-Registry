0ca382007acd870f2d8a4eaf7f68faafe9bc0c08
Justin Herrick <justin@justinherrick.com>
2011-10-08 18:05:52 -0400

    a few css tweaks

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+
+  def current_url(overwrite={})
+    url_for :only_path => false, :params => params.merge(overwrite)
+  end
+
+  def has_permission?
+    if current_user.id == @user.id
+      true
+    else
+      false
+    end
+  end
+
+
 end
