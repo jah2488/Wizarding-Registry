7f34b517cd1318971d9d80d88d24e8c2431ba3fc
Justin Herrick <justin@justinherrick.com>
2011-10-20 02:57:45 -0400

    refactoring

--- app/presenters/user_presenter.rb
+++ app/presenters/user_presenter.rb
@@ -46,36 +46,27 @@ class UserPresenter
       url = "https://plus.google.com/#{@user.google_plus}/posts"
     when "youtube"
       url = "http://youtube.com/#{@user.youtube.to_s}"
-    when "email"
-      url = "mailto:#{@user.email}"
-    when "site"
-      url = @user.site.gsub(/http(.)\/\//, "")
-    when "site2"
-      url = @user.site2.gsub(/http(.)\/\//, "")
     end
     link_handler(type, url)
   end
 
   def link_handler(type, url)
     handle_link "#{type}", eval("@user.#{type}") do
-      h.link_to "", url, id: "#{type}"    end
+      h.link_to "", url, id: "#{type}"    
+    end
+  end
+  
+  def site_link(type)
+    attribute = eval("@user.#{type}")
+    url = @user.attribute.gsub(/http:\/\//, "")
+    handle_link "#{attribute}", attribute  do
+      h.link_to "", "http://#{url}", id: attribute, confirm: "Just to let you know. We cannot guarantee a death eater did not curse this link with computer viruses or Pr0n. Are you sure you trust this wizard?"
+    end
   end
 
   def email
     handle_link "email", @user.email do
-      h.link_to "", "mailto:#{@user.email}" , id: "email", confirm: "Just to let you know. We cannot guarantee a death eater did not curse this link with computer viruses or Pr0n. Are you sure you trust this wizard?"   
-    end
-  end
-
-  def site
-    handle_link "site", @user.site do
-      h.link_to "" , "http://#{@user.site}" , id: "site", confirm: "Just to let you know. We cannot guarantee a death eater did not curse this link with computer viruses or Pr0n. Are you sure you trust this wizard?"
-    end
-  end
-
-  def site2
-    handle_link "site", @user.site2 do
-      h.link_to "" , "http://#{@user.site2}" , id: "site", confirm: "Just to let you know. We cannot guarantee a death eater did not curse this link with computer viruses or Pr0n. Are you sure you trust this wizard?"
+      h.link_to "", "mailto:#{@user.email}" , id: "email", confirm: "Just so you know. This link will email your fellow wizard. Beware Death Eaters in Desquise."   
     end
   end
 
